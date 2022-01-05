176172ab7dbe07eb7a0b374be3110dd0b7024fa0
James Harris <contact@jamesharris.id.au>
2022-01-05 18:17:47 +1000

    Clone using the auth method specified in the configuration.

--- cmd/gritd/internal/source/github/clone.go
+++ cmd/gritd/internal/source/github/clone.go
@@ -5,6 +5,10 @@ import (
 
 	"github.com/dogmatiq/dodeca/logging"
 	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/go-git/go-git/v5/plumbing/transport/ssh"
+	"github.com/google/go-github/github"
+	"github.com/gritcli/grit/internal/config"
 )
 
 // Clone makes a repository available at the specified directory.
@@ -33,20 +37,65 @@ func (s *impl) Clone(
 
 	logging.Debug(serverLog, "cloning %s to %s", r.GetFullName(), dir)
 
+	opts, err := newCloneOptions(
+		s.cfg,
+		r,
+		logging.Tee(
+			logging.Demote(serverLog), // log to the server as debug
+			clientLog,                 // log to the client as regular message
+		),
+	)
+	if err != nil {
+		logging.Log(serverLog, "unable to construct clone options: %w", err)
+		return err
+	}
+
 	_, err = git.PlainCloneContext(
 		ctx,
 		dir,
 		false, // isBare
-		&git.CloneOptions{
-			URL: r.GetSSHURL(),
-			Progress: &logging.LineWriter{
-				Target: logging.Tee(
-					logging.Demote(serverLog), // log to the server as debug
-					clientLog,                 // log to the client as regular message
-				),
-			},
-		},
+		opts,
 	)
 
 	return err
+}
+
+// newCloneOptions returns new clone options based on source configuration.
+func newCloneOptions(
+	cfg config.GitHub,
+	r *github.Repository,
+	logger logging.Logger,
+) (*git.CloneOptions, error) {
+	opts := &git.CloneOptions{
+		URL: r.GetSSHURL(),
+		Progress: &logging.LineWriter{
+			Target: logger,
+		},
+	}
+
+	if cfg.Git.PreferHTTP {
+		opts.URL = r.GetCloneURL()
+
+		if cfg.Token != "" {
+			opts.Auth = &http.BasicAuth{
+				Username: "token", // ignored
+				Password: cfg.Token,
+			}
+		}
+	}
+
+	if cfg.Git.SSHKeyFile != "" {
+		publicKeys, err := ssh.NewPublicKeysFromFile(
+			"git", // github always uses the "git" username for SSH access
+			cfg.Git.SSHKeyFile,
+			cfg.Git.SSHKeyPassphrase,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		opts.Auth = publicKeys
+	}
+
+	return opts, nil
 }
