beb3d2e272f39c5668706157318ff9c91f93fa69
James Harris <contact@jamesharris.id.au>
2022-01-09 18:57:30 +1000

    Add `source.Source.CloneDir`.

--- cmd/gritd/internal/source/source.go
+++ cmd/gritd/internal/source/source.go
@@ -14,6 +14,10 @@ type Source struct {
 	// Description is a human-readable description of the source.
 	Description string
 
+	// CloneDir is the directory containing repositories cloned from this
+	// source.
+	CloneDir string
+
 	// Driver is the driver used to perform repository operations.
 	Driver Driver
 }
