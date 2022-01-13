1d7bcab14d4625090bb2d62b241a85e9c561f7ed
James Harris <contact@jamesharris.id.au>
2022-01-13 20:01:36 +1000

    Remove `sourcebuilder` package.

--- internal/daemon/internal/source/sourcebuilder/doc.go
+++
@@ -1,4 +0,0 @@
-// Package sourcebuilder constructs the source.Source and driver.Driver values
-// used by the daemon from the config.Source definitions in the Grit
-// configuration.
-package sourcebuilder
