46f9d31a3884add3a91795fb07a9424e4f765b31
James Harris <contact@jamesharris.id.au>
2022-01-17 16:17:18 +1000

    Remove Git VCS driver-specific tests from githubsource tests.

--- driver/sourcedriver/githubsource/config_test.go
+++ driver/sourcedriver/githubsource/config_test.go
@@ -1,7 +1,6 @@
 package githubsource_test
 
 import (
-	"github.com/gritcli/grit/config"
 	"github.com/gritcli/grit/driver/configtest"
 	. "github.com/gritcli/grit/driver/sourcedriver/githubsource"
 	. "github.com/onsi/ginkgo"
@@ -30,7 +29,7 @@ var _ = Describe("type Config", func() {
 	})
 })
 
-var _ = Describe("configuration integration", func() {
+var _ = Describe("type configSchema", func() {
 	configtest.TestSourceDriver(
 		Registration,
 		configtest.SourceSuccess(
@@ -51,103 +50,6 @@ var _ = Describe("configuration integration", func() {
 			Config{
 				Domain: "github.example.com",
 			},
-		),
-		configtest.SourceSuccess(
-			"explicit SSH key",
-			`source "github" "github" {
-				git {
-					ssh_key {
-						file = "/path/to/key"
-					}
-				}
-			}`,
-			Config{
-				Domain: "github.com",
-				Git: config.Git{
-					SSHKeyFile: "/path/to/key",
-				},
-			},
-		),
-		configtest.SourceSuccess(
-			"explicit SSH key with passphrase",
-			`source "github" "github" {
-				git {
-					ssh_key {
-						file = "/path/to/key"
-						passphrase = "<passphrase>"
-					}
-				}
-			}`,
-			Config{
-				Domain: "github.com",
-				Git: config.Git{
-					SSHKeyFile:       "/path/to/key",
-					SSHKeyPassphrase: "<passphrase>",
-				},
-			},
-		),
-		configtest.SourceSuccess(
-			"does not inherit default passphrase when SSH key is specified explicitly",
-			`git {
-				ssh_key {
-					file = "/path/to/key"
-					passphrase = "<passphrase>"
-				}
-			}
-
-			source "github" "github" {
-				git {
-					ssh_key {
-						file = "/path/to/different/key"
-					}
-				}
-			}`,
-			Config{
-				Domain: "github.com",
-				Git: config.Git{
-					SSHKeyFile:       "/path/to/different/key",
-					SSHKeyPassphrase: "", // note: different to default git config
-				},
-			},
-		),
-		configtest.SourceSuccess(
-			"explicitly prefer SSH",
-			`source "github" "github" {
-				git {
-					prefer_http = false
-				}
-			}`,
-			Config{
-				Domain: "github.com",
-				Git: config.Git{
-					PreferHTTP: false,
-				},
-			},
-		),
-		configtest.SourceSuccess(
-			"explicitly prefer HTTP",
-			`source "github" "github" {
-				git {
-					prefer_http = true
-				}
-			}`,
-			Config{
-				Domain: "github.com",
-				Git: config.Git{
-					PreferHTTP: true,
-				},
-			},
-		),
-		configtest.SourceFailure(
-			`explicit SSH passphrase without key file`,
-			`source "github" "github" {
-				git {
-					ssh_key {
-						passphrase = "<passphrase>"
-					}
-				}
-			}`,
-			`<dir>/config-0.hcl:3,14-14: Missing required argument; The argument "file" is required, but no definition was found.`,
 		),
 	)
 })
