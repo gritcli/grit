1353e9088d7fb27aa8a54886d71ec42dda6f22b9
James Harris <contact@jamesharris.id.au>
2022-01-27 18:18:15 +1000

    Move `di` package into `internal/`.

--- cli/internal/deps/container.go
+++ cli/internal/deps/container.go
@@ -1,6 +1,6 @@
 package deps
 
-import "github.com/gritcli/grit/internal/common/di"
+import "github.com/gritcli/grit/internal/di"
 
 // Container is the dependency injection container for the Grit CLI.
 var Container di.Container
